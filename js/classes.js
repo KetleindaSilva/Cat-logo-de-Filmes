98abe1795a2ef44ab21e20a13e3b5344d2781e08
Ketlein <ketleindasilva2020@gmail.com>
2023-03-28 20:25:02 -0300

    finalização da parte 3

--- js/classes.js
+++ js/classes.js
@@ -1,117 +1,161 @@
 class Ator{
-    constructor(id,nome){
-        this.nome=nome;
-        this.id=id;
+    constructor(id, nome){
+        this.nome = nome;
+        this.id = id;
     }
 }
+
 class Diretor{
-    constructor(id,nome){
-        this.nome=nome;
-        this.id=id;
+    constructor(id, nome){
+        this.nome = nome;
+        this.id = id;
     }
 }
+
+
 class Filme{
-    constructor(id,titulo,ano,genero,duracao,cartaz,sinopse,direcao,elenco,classificacao,avaliacao){
-        this.id=id;
-        this.titulo=titulo;
-        this.ano=ano;
-        this.genero=genero;
-        this.duracao=duracao;
-        this.sinopse=sinopse;
-        this.cartaz=cartaz;
-        this.direcao=direcao;
-        this.elenco=elenco;
-        this.classificacao=classificacao;
-        this.avaliacao=avaliacao;
-        this.btnDetalhes=null;
+    constructor(id, titulo, ano, genero, duracao, sinopse, cartaz, direcao, elenco, classificacao, avaliacao){
+        this.id = id;
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.cartaz = cartaz;
+        this.direcao = direcao;
+        this.elenco = elenco;
+        this.classificacao = classificacao;
+        this.avaliacao = avaliacao;
+        this.btnDetalhes = null;
     }
-    setBtnDetalhes=() =>{
-        this.btnDetalhes =document.createElement('button');
-        this.btnDetalhes.appendChild(document.createTextNode("Detalhes"));
-        this.btnDetalhes.setAttribute("id",this.id);
-        this.btnDetalhes.setAttribute("class","btnDetalhesFilme");
-    }
-    getBtnDetalhes=()=>{
-        return this.btnDetalhes
-    }
-    getDetalhesFilmes = async ()=>{
 
-        let imgCartaz = document.createElement("img");
-        imgCartaz.setAttribute("class", "card-img"); 
-        imgCartaz.setAttribute("src", this.cartaz);
+    getCard = async () => {
 
-        let cardBody = document.createElement("div"); 
-        cardBody.setAttribute("class","card-detalhes");
-
-        let divDetalhes = document.createElement("div"); 
-        divDetalhes.setAttribute("class","informacoes" );
-
-        let divGenero = document.createElement("div");
-        divGenero.setAttribute("style", "flex-grow:1;");
-
-        let divAnoProducao = document.createElement("div"); 
-        divAnoProducao.setAttribute("style", "flex-grow:1;"); 
-        
-        let detalhes = document.createElement("div");
-        detalhes.setAttribute("class", "card mb-3");
-        detalhes.setAttribute("class", "row");
-        let imagem = document.createElement("div");
-        imagem.setAttribute("class", "col-mb-4");
-        let img = document.createElement("img");
-        img.setAttribute("class", "img-fluid");
-        img.setAttribute("style", "rounded-start")
-        let cardc  = document.createElement("div");
-        cardc.setAttribute("class","col-md-8");
-        let cardbody = document.createElement("div");
-        cardbody.setAttribute("class","card-body");
-        let cardh5 = document.createElement("h5");
-        cardh5.setAttribute("class","card-title");
-        let cardp = document.createElement("p");
-        cardp.setAttribute("class","card-text");
-        let cardpp = document.createElement("p");
-        cardpp.setAttribute("class","card-text");
-        let small = document.createElement("small");
-        small.setAttribute("class","text-muted");
-        cardh5.appendChild(document.createTextNode(this.titulo));
-        
-    }
-    getCard = async ()=>{
         let card = document.createElement("div");
         card.setAttribute("class", "card");
+
         let imgCartaz = document.createElement("img");
         imgCartaz.setAttribute("class", "card-img-topz");
         imgCartaz.setAttribute("src", this.cartaz);
+
         let cardBody = document.createElement("div");
         cardBody.setAttribute("class", "card-body");
+
         let hCardTitle = document.createElement("h5");
         hCardTitle.setAttribute("class", "card-title");
+
         let divDetalhes = document.createElement("div");
         divDetalhes.setAttribute("style", "display: flex; justify-content: space-around; text-align: center; font-weight: 700;");
+
         let divGenero = document.createElement("div");
         divGenero.setAttribute("style", "flex-grow:1;");
+
         let divAnoProducao = document.createElement("div");
         divAnoProducao.setAttribute("style", "flex-grow:1;");
+
         let divClassificacao = document.createElement("div");
         divClassificacao.setAttribute("style", "flex-grow:1;");
+
         hCardTitle.appendChild(document.createTextNode(this.titulo));
         divGenero.appendChild(document.createTextNode(this.genero));
         divAnoProducao.appendChild(document.createTextNode(this.ano));
         divClassificacao.appendChild(document.createTextNode(this.classificacao));
+
         divDetalhes.appendChild(divGenero);
         divDetalhes.appendChild(divAnoProducao);
         divDetalhes.appendChild(divClassificacao);
+
         card.appendChild(imgCartaz);
         card.appendChild(cardBody);
+
         cardBody.appendChild(hCardTitle);
         cardBody.appendChild(divDetalhes);
 
-
         this.setBtnDetalhes();
         cardBody.appendChild(this.getBtnDetalhes());
-
+    
         return card;
     }
-    
+
+    getBtnDetalhes = () => {
+
+        return this.btnDetalhes;
+
+    }
+
+    setBtnDetalhes = () => {
+
+        this.btnDetalhes = document.createElement('button');
+        this.btnDetalhes.appendChild(document.createTextNode("Detalhes"));
+        this.btnDetalhes.setAttribute("id", this.id);
+        this.btnDetalhes.setAttribute("class", "btnDetalhesFilme");
+
+    }
+
+    getDetalhesFilme = () => {
+
+        let cardDet = document.createElement('div');
+        cardDet.setAttribute("class", "carddetalhes");
+
+        let imgcard = document.createElement("img");
+        imgcard.setAttribute("class", "imagemCardDetalhes");
+        imgcard.setAttribute("src", this.cartaz);
+
+        let BodyDetalhes = document.createElement("div");
+        BodyDetalhes.setAttribute("class", "body-detalhes");
+        let h5TituloDetalhes = document.createElement("h5");
+        h5TituloDetalhes.setAttribute("class", "card-title");
+        h5TituloDetalhes.appendChild(document.createTextNode(this.titulo));
+        let divDetalhes = document.createElement("div");
+        divDetalhes.setAttribute("class", "detalhescard");
+        let divAno = document.createElement("div");
+        divAno.setAttribute("class", "anocard");
+        let divGenero = document.createElement("div");
+        divGenero.setAttribute("class", "generocard");
+        let divDuracao = document.createElement("div");
+        divDuracao.setAttribute("class", "duracaocard");
+        let divDirecao = document.createElement("div");
+        divDirecao.setAttribute("class", "direcaocard");
+        let divElenco = document.createElement("div");
+        divElenco.setAttribute("class", "elencocard");
+        let divAvaliacao = document.createElement("div");
+        divAvaliacao.setAttribute("class", "avaliacaocard");
+        let divSinopse = document.createElement("div");
+        divSinopse.setAttribute("class", "sinopsecard");
+
+        divAno.appendChild(document.createTextNode(this.ano));
+        divGenero.appendChild(document.createTextNode(this.genero));
+        divDuracao.appendChild(document.createTextNode(this.duracao));
+        divDirecao.appendChild(document.createTextNode(this.direcao));
+        divElenco.appendChild(document.createTextNode(this.elenco));
+        divAvaliacao.appendChild(document.createTextNode(this.avaliacao));
+        divSinopse.appendChild(document.createTextNode(this.sinopse));
+
+        divDetalhes.appendChild(divAno);
+        divDetalhes.appendChild(divGenero);
+        divDetalhes.appendChild(divDuracao);
+        divDetalhes.appendChild(divDirecao);
+        divDetalhes.appendChild(divElenco);
+        divDetalhes.appendChild(divAvaliacao);
+        divDetalhes.appendChild(divSinopse);
+        cardDet.appendChild(imgcard);
+        cardDet.appendChild(BodyDetalhes);
+        BodyDetalhes.appendChild(h5TituloDetalhes);
+        BodyDetalhes.appendChild(divDetalhes);
+
+        let btnSalvar = document.createElement('button');
+        btnSalvar.appendChild(document.createTextNode('Salvar'));
+        btnSalvar.setAttribute('id', 'btnSalvar');
+        divDetalhes.appendChild(btnSalvar);
+
+        let btnFechar = document.createElement('button');
+        btnFechar.appendChild(document.createTextNode('Fechar'));
+        btnFechar.setAttribute('id', 'btnFechar');
+        divDetalhes.appendChild(btnFechar);
+
+        return cardDet;
+
+    }
 }
 /*class Detalhes{
     constructor(titulo,ano,id,tipo,poster){
--- js/script.js
+++ js/script.js
@@ -1,84 +1,87 @@
 
-let inputBuscarFilme= document.querySelector("#input-buscar-filme");
-let btnBuscarFilme= document.querySelector("#btn-buscar-filme");
+let inputBuscarFilme = document.querySelector("#input-buscar-filme");
+let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
+let mostrarFilme = document.querySelector("#mostrar-filme");
+let listaFilme = document.querySelector("#lista-filme");
 
-btnBuscarFilme.onclick = () =>{
+btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.length > 0){
         let filmes = new Array();
         fetch("http://www.omdbapi.com/?apikey=e539ebf5&s="+inputBuscarFilme.value, {mode: "cors"})
-        .then((resp)=> resp.json())
-        .then((resp)=> {
-            console.log(resp)
-            resp.Search.forEach((item)=>{
+        .then((resp) => resp.json())
+        .then((resp) => {
+            resp.Search.forEach(item => {
                 console.log(item);
-                let filme= new Filme(
+                let filme = new Filme(
                     item.imdbID,
                     item.Title,
                     item.Year,
+                    null,
                     null,
                     null,
                     item.Poster,
                     null,
                     null,
                     null,
-                    null,
-                    null,   
+                    null
                 );
                 filmes.push(filme);
             });
             listarFilmes(filmes);
-            listarFilmes(filmes);
         })
+    
     }
+    mostrarFilme.style.display = "none";
     return false;
 }
 
 
 
-let detalhesFilme = async (id)=>{
-    fetch("http://www.omdbapi.com/?apikey=e539ebf5&i="+id)
-    .then((resp)=> resp.json())
-    .then((resp)=>{
-        let filme = new Filme(
-            resp.imdbID,
-            resp.Title,
-            resp.Year,
-            resp.Genre.split(","),
-            resp.Runtime,
-            resp.Poster,
-            resp.Plot,
-            resp.Director,
-            resp.Actors.split(","),
-            resp.Director,
-            resp.Awards,
-            resp.imdbRating
-        );
-        document.querySelector("#mostrar-filme").appendChild(filme.getDetalhesFilme());
-        document.querySelector("#lista-filmes").style.display="none";
-        document.querySelector("#mostrar-filme").style.display="flex";
-        
-    });
-}
-let listarFilmes = async (filmes)=> {
-    let listaFilmes = await document.querySelector("#lista-filmes");
-    listaFilmes.innerHTML="";
-    mostrarFilme.innerHTML = "";
-    document.querySelector("#mostrar-filme").innerHTML = "";
-    document.querySelector("#mostrar-filme").style.display = "none";
-    
+let detalhesFilme = async (id) => {
+    fetch("http://www.omdbapi.com/?apikey=e539ebf5&i=" + id)
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            let filme = new Filme(
+                resp.imdbID,
+                resp.Title,
+                resp.Year,
+                resp.Genre.split(", "),
+                resp.Runtime,
+                resp.Plot,
+                resp.Poster,
+                resp.Director,
+                resp.Actors.split(", "),
+                resp.Awards,
+                resp.imdbRating
+            )
+            mostrarFilme.style.display = "flex";
+            mostrarFilme.appendChild(filme.getDetalhesFilme());
 
+            
+        });
+}
+
+
+
+let listarFilmes = async (filmes) => {
+    let listaFilmes = await document.querySelector("#lista-filmes");
+    listaFilmes.style.display = "flex";
+    listaFilmes.innerHTML = "";
+    document.querySelector("#mostrar-filme").innerhtml = "";
     if (filmes.length > 0) {
         filmes.forEach(async(filme) => {
             console.log(filme);
-            listaFilmes.appendChild( await filme.getCard());
-            filme.getBtnDetalhes().onclick=()=>{
+            listaFilmes.appendChild(await filme.getCard());
+            filme.getBtnDetalhes().onclick = () => {
+                listaFilmes.style.display="none";
                 detalhesFilme(filme.id);
             }
         });
     }
-
 }
+
 
 
 /*
